67f2be10530948df4b404ad6797e8ec8c90284d3
ardevr <30952316+ardevr@users.noreply.github.com>
2017-08-12 13:17:19 +0200

    Create MAB.py

---
+++ environnement/MAB.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+''' Environnement for a multi-armed bandit problem with side observations. Inspired by the work of O.Cappé and A.Garivier'''
+__author__ = "Garcelon, Evrard"
+
+import numpy.random
+import numpy as np
+from environnement.Results import *
+class MAB:
+    def __init__(self,arms,Delta):
+        self.arms = arms
+        self.nbArms = len(arms)
+        self.Delta = Delta
+    
+    def play(self,policy,epsilon,T):
+        policy.startGame()
+        result = Result(self.nbArms,self.Delta,T)
+        for t in range(T):
+            Z = int((np.random.rand(1) <= (epsilon)) * 1)
+            It = policy.choice() #Choice of the arm played at t
+            reward1 = self.arms[It].draw()
+            result.store(t, It)
+            if Z==1 and t>=self.nbArms:
+                Jt = policy.secondChoice() #Choice of the second arm played at t if the player has access to a second observation that does not impact the regret 
+                if Jt != It:
+                    reward2 = self.arms[Jt].draw()
+                    policy.getRewardDouble(t,It, Jt, reward1, reward2)
+            else :
+                policy.getReward(t,It, reward1)
+        return result
+                
+            
