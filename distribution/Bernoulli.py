1672b3bd1a73d0237f5466efd22607eff4bfa9af
ardevr <30952316+ardevr@users.noreply.github.com>
2018-05-20 21:10:50 +0200

    Create Bernoulli.py

---
+++ distribution/Bernoulli.py
@@ -0,0 +1,11 @@
+import numpy as np
+class Bernoulli():
+
+    def __init__(self, p): 
+        self.p = p
+        self.expectation = p
+        
+    def draw(self):
+        return (np.random.rand(1) <= (self.p)) * 1.
+    def expectation(self):
+        return self.p
